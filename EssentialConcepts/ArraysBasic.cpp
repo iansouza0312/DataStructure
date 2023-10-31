8917b7a7f3ab5ea2ce2c1e0403baf4fe371117dc
Ian Souza <ianfmsouza@gmail.com>
2023-10-30 23:36:12 -0300

    first commit

---
+++ EssentialConcepts/ArraysBasic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+	int A[10]={2, 4, 6, 8, 10, 12, 14};
+
+	cout<<sizeof(A)<<endl;
+	cout<<A[1]<<endl;
+	
+	printf("%d\n", A[2]);
+	
+	return 0;
+}
